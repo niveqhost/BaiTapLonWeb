8c81ba51a15fc199b953dd6961b7475da04290a5
Tran Hai <khoidaumoichotoi@gmail.com>
2021-05-04 16:33:43 +0700

    Them chuc nang cho phan doc tin tuc

---
+++ DocTinTuc/WebDocTinTuc/Controllers/TheLoaiController.cs
@@ -0,0 +1,26 @@
+﻿using Model.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDocTinTuc.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private PostDao dao = new PostDao();
+        // GET: TheLoai
+        public ActionResult Index(string UrlRequire, int page = 1, int pageSize = 10)
+        {
+            
+            var model = dao.ListAllPostForType(UrlRequire, page, pageSize);
+            //top 10 xem nhieu
+            ViewBag._k_relative = new PostDao().top10VIew();
+
+            //Lay ten the loai
+            ViewBag.vbtentheloai = dao.theloaibaidangtentheloai(UrlRequire);
+            return View(model);
+        }
+    }
+}
